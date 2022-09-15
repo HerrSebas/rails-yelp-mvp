a63b0953d9a6eef73d69243c10f7fef55e741f92
Sebastian Naranjo <sebnartor@gmail.com>
2022-09-15 18:41:53 -0500

    completed

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,9 @@
 class ReviewsController < ApplicationController
 
-  def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-  end
+  # def new
+  #   @restaurant = Restaurant.find(params[:restaurant_id])
+  #   @review = Review.new
+  # end
 
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
@@ -13,11 +13,11 @@ class ReviewsController < ApplicationController
     if @review.save
       redirect_to restaurant_path(@restaurant)
     else
-      render :new, status: :unprocessable_entity
+      render 'restaurants/show', status: :unprocessable_entity
     end
   end
 
   def review_params
-    params.require(:review).permit([:rating, :content])
+    params.require(:review).permit(:rating, :content)
   end
 end
